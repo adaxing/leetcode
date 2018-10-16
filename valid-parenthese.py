769e38c05f49a48e1fb2f3d4b97212bd8a50707f
adaxing <xing2@ualberta.ca>
2018-10-16 11:57:55 -0600

    implementation of stack to solve valid parenthese

--- valid-parenthese.py
+++ valid-parenthese.py
@@ -1,31 +1,44 @@
 from pythonds.basic.stack import Stack
 
 def isValid(s):
+	# dic = {
+	# 	'(':')',
+	# 	'{':'}',
+	# 	'[':']',
+	# }
+	# st = Stack()
+	# match = True
+	# for para in s:
+	# 	if para in dic.keys():
+	# 		st.push(dic[para])
+	# 	else:
+	# 		if para != st.pop():
+	# 			match = False
+
+	# if st.isEmpty() and match:
+	# 	return True
+	# else:
+	# 	return False
 	dic = {
 		'(':')',
 		'{':'}',
 		'[':']',
-	}
-	st = Stack()
-	balanced = True
-	index = 0
-	print(len(s))
-	while index < len(s) and balanced:
-		symbol = s[index]
-		if symbol == dic.keys():
-			st.push(symbol)
-		else:
-			print(st.isEmpty())
-			if st.isEmpty():
-				balanced = False
-			else:
-				print("!!!")
-				match = dic[symbol]=st.pop()
-		index = index + 1
-	if balanced and st.isEmpty():
-		return True
-	else: 
-		return False
+        }
+        stack = []
+        match = True
+        for para in s:
+            if para in dic.keys():
+                stack.append(dic[para])
+            else:
+                if len(stack) == 0:
+                    return False
+                elif len(stack) != 0 and para != stack.pop():
+                    match = False
+
+        if len(stack) == 0 and match:
+            return True
+        else:
+            return False
 
    
 input_value = input("Input:")
