18588798fd758bbb1407af5a9a56463883aad7cf
adaxing <xing2@ualberta.ca>
2018-10-19 13:28:33 -0600

    done

--- merge-two-sorted-list.py
+++ merge-two-sorted-list.py
@@ -1,21 +1,27 @@
-def mergeTwoLists(l1, l2):
-    if l1 is None:
-        return l2
-    if l2 is None:
-        return l1
-    result_node = []
-    l1_cur_val = l1.value
-    l2_cur_val = l2.value
-    next_val = l1.next
-    print(next_val)
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result_list = ListNode(1)
+        trail = result_list
+        while (l1 and l2):
+            if l1.val <= l2.val:
+                trail.next = l1
+                l1 = l1.next
+                trail = trail.next
+            else: 
+                trail.next = l2
+                l2 = l2.next
+                trail = trail.next
+        if l1:
+            trail.next = l1
+        elif l2:
+            trail.next = l2
+        return result_list.next
+    
 
-    if l1_cur_val < l2_cur_val:
-        result_node.append(l1_cur_val)
-        l1_cur_val = l1.next
-    else: 
-        result_node.append(l2_cur_val)
-        l2_cur_val = l2.next          
 
-    print(result_node)
 
-mergeTwoLists([1,2,4],[2,3,6])
